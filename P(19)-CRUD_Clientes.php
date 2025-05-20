995786e6808ac2509cee2db1dfef7424161f21dc
MissaelCeja <cejamissael0@gmail.com>
2025-05-19 22:50:24 -0600

    Correccion
    Linkear css

--- P(19)-CRUD_Clientes.php
+++ P(19)-CRUD_Clientes.php
@@ -4,8 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>CRUD Clientes</title>
-  <link rel="stylesheet" href="">
+  <link rel="stylesheet" href="P(19)_CSS.css">
 </head>
+
 <body>
   <header class="navbar">
     <div class="logo">QuintaViva</div>
