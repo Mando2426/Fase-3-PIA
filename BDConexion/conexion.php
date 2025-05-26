049be8a97b98fd5b87a478fc4ac59b4ca1d5f9c6
MissaelCeja <cejamissael0@gmail.com>
2025-05-26 10:55:53 -0600

    Pruebas de conexion
    Base de datos

---
+++ BDConexion/conexion.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "Fase2BD"; 
+
+$conexion = new mysqli($servername, $username, $password, $dbname);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+?>
+
